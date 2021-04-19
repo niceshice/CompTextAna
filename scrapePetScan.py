c32403b8a83ca44644a5f393ee2fb51298c4a66c
niceshice <ju.scheuchenpflug@gmail.com>
2021-04-19 14:44:12 +0200

    added script for scraping PetScan
    PetScan gets full list of items in a category, not cut list from wikipedia

--- scrapePetScan.py
+++ scrapePetScan.py
@@ -2,8 +2,8 @@ import requests
 import os.path
 from bs4 import BeautifulSoup
 
-repoPath = "D:\\cravi\\Documentsold\\Uni\\DH\\CompTextAna\\Korpus\\"
-categoryName = "Doku-Drama"
+repoPath = "D:\\cravi\\Documentsold\\Uni\\DH\\CompTextAna\\Testkorpus\\"
+categoryName = "Western"
 
 try:
     os.mkdir(os.path.join(repoPath, categoryName))
@@ -16,7 +16,7 @@ def collectLinks(url):
     soup = BeautifulSoup(response.content, "html.parser")
     
     # get all links from categories page
-    allLinks = soup.find(id="mw-pages").find_all("a")
+    allLinks = soup.find(id="main_table").find_all("a")
     
     # get content for each link
     for link in allLinks:
@@ -24,7 +24,7 @@ def collectLinks(url):
         if link["href"].find("/wiki/") == -1:
             continue
         # concat to full url
-        getContent("https://de.wikipedia.org" + link["href"])
+        getContent(link["href"])
     else:
         print("Finished!")
 
@@ -34,13 +34,14 @@ def getContent(link):
     soup = BeautifulSoup(response.content, "html.parser")
     
     # get title for filename
-    title = soup.find(id="firstHeading")
+    title = formatTitle(soup.find(id="firstHeading"))
+    print(type(title), title)
     
     # get text content and prettify
     content = prettifyContent(soup.find(id="mw-content-text"))
     
     # write to file
-    outFile = open(os.path.join(repoPath, categoryName, title.text + ".txt"), "w", encoding="utf8")
+    outFile = open(os.path.join(repoPath, categoryName, title + ".txt"), "w", encoding="utf8")
     outFile.write(content)
     
     outFile.close()
@@ -48,6 +49,15 @@ def getContent(link):
     
 def prettifyContent(content):
     return content.text
+
+def formatTitle(title):
+    unwanted = '<>:"/\|?*'
+    title = title.text
+    for char in title:
+        if char in unwanted and char in title:
+            title.replace(char, "!")
+            print(title)
+    return title
     
 # start pulling from this link
-collectLinks("https://de.wikipedia.org/wiki/Kategorie:" + categoryName)
+collectLinks("https://petscan.wmflabs.org/?ns[0]=1&ns[100]=1&project=wikipedia&categories=" + categoryName + "&depth=10&language=de&ns[6]=1&sortby=ns_title&interface_language=de&ns[10]=1&doit=&interface_language=de")
