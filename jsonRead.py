5e8a62c56deb3782221a33acfd1cb44fffca0f89
niceshice <ju.scheuchenpflug@gmail.com>
2021-04-29 13:26:25 +0200

    added simple JSON readout

---
+++ jsonRead.py
@@ -0,0 +1,11 @@
+import os
+import json
+
+
+def read(doc):
+    with open(doc, "r", encoding="utf8") as f:
+        output = json.loads(f.read())["text"]
+        return output
+
+string = read(r"D:\cravi\Documentsold\Uni\DH\CompTextAna\Korpus\Thriller\13_sins.json")
+print(string)
