8be7d3e9a58d1b00710dea195336584c1a5f698e
niceshice <ju.scheuchenpflug@gmail.com>
2021-05-25 19:45:24 +0200

    added script for getting bag of words per document

--- bow.py
+++ bow.py
@@ -3,18 +3,19 @@ import json
 import spacy
 from tqdm import tqdm
 
-repo = "./Testkorpus/"
+repo = "./Korpus/"
+#repo = "./Testkorpus/Test/"
+bow_repo = "./BoWs/"
+#bow_repo = "./Testkorpus/BoW/"
 nlp = spacy.load("de_core_news_sm")
-lemmas = set()
 
 for doc in tqdm(os.listdir(repo)):
+    lemmas = set()
     with open(f"{repo}{doc}", "r", encoding="utf8") as f:
         tempdict = json.loads(f.read())
         text = nlp(tempdict["text"])
         for word in text:
             lemmas.add(word.lemma_)
 
-    with open(f"./BoWs/{doc}.json", "w", encoding="utf8") as f:
-        f.write(str(lemmas))
-
-
+    with open(f"{bow_repo}{doc}", "w", encoding="utf8") as f:
+        f.write(json.dumps(list(lemmas)))
