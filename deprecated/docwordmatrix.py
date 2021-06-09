a4ac63026b4d11342389d6068e575e5602009a2c
niceshice <ju.scheuchenpflug@gmail.com>
2021-06-09 23:41:41 +0200

    moved to deprecated, it's in the notebook now

--- deprecated/docwordmatrix.py
+++ deprecated/docwordmatrix.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from plotly import express
 from glob import glob
+import seaborn as sns
 
 repo = "./Korpus/"
 
@@ -12,14 +13,23 @@ files = sorted(glob(glob_pattern), key=os.path.getctime)
 
 vec = CountVectorizer(input="filename")
 X = vec.fit_transform(files)
+print("X before trans", X)
 X = TfidfTransformer(use_idf=True).fit_transform(X)
-print(X.shape)
+print("X.shape", X.shape)
+print("X", X)
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=3)
 x2d = pca.fit_transform(X.toarray())
+print("x2d", x2d)
 
 x_axis = x2d[:,0]
+print("x_axis", x_axis)
 y_axis = x2d[:,1]
+print("y_axis", y_axis)
+z_axis = x2d[:,2]
+print("z_axis", z_axis)
 
-plot = express.scatter(x=x_axis, y=y_axis)
-plot.show()
+sns.scatterplot(x=x_axis, y=y_axis)
+
+#plot = express.scatter_3d(x=x_axis,y=y_axis,z=z_axis)
+#plot.show()
